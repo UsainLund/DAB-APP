dbeb5fe1a672152dfbb7d7993ac09dadbbb628e1
markchr98 <mark4@live.dk>
2016-04-07 09:14:25 +0200

    collision

--- Test.java
+++ Test.java
@@ -4,5 +4,19 @@ public class Test
 	{
 		//Tester Github.
 		Christian har testet
+		public class Collision {
+
+		    public void testMethod()
+    			{
+        			Rect rect1 = new Rect();
+        			Rect rect2 = new Rect();
+
+        			if (rect1.getX()<rect2.getX()+rect2.getWidth()&&
+                			rect1.getX()+rect1.getWidth()>rect2.getX()&&
+                			rect1.getY()<rect2.getY()+rect2.getHeight()&&
+                			rect1.getHeight()+rect1.getY()>rect2.getY())
+        				{
+
+				        };
+			}
 	}
-}
