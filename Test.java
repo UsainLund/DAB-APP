bf5e62f51dab23ec121da6b6dfe72eaf402ac667
markchr98 <mark4@live.dk>
2016-04-07 12:46:34 +0200

    collision

--- Test.java
+++ Test.java
@@ -3,20 +3,31 @@ public class Test
 	public static void main(String[] args)
 	{
 		//Tester Github.
-		Christian har testet
-		public class Collision {
+		Christian har testet		
 
-		    public void testMethod()
-    			{
-        			Rect rect1 = new Rect();
-        			Rect rect2 = new Rect();
+		   public void onClick(View v) 
+		   {
+       			Rect rect1 = new Rect();
+        		Rect rect2 = new Rect();
+        		rect1.setHeight(5);
+        		rect1.setWidth(8);
+        		rect1.setX(5);
+        		rect1.setY(10);
+        		rect2.setHeight(10);
+        		rect2.setWidth(10);
+       			rect2.setX(12);
+        		rect2.setY(14);
 
-        			if (rect1.getX()<rect2.getX()+rect2.getWidth()&&
-                			rect1.getX()+rect1.getWidth()>rect2.getX()&&
-                			rect1.getY()<rect2.getY()+rect2.getHeight()&&
-                			rect1.getHeight()+rect1.getY()>rect2.getY())
-        				{
-
-				        };
-			}
-	}
+        		if (rect1.getX()<rect2.getX()+rect2.getWidth()&&
+                		rect1.getX()+rect1.getWidth()>rect2.getX()&&
+               			rect1.getY()<rect2.getY()+rect2.getHeight()&&
+                		rect1.getHeight()+rect1.getY()>rect2.getY())
+        		{
+            			((TextView)findViewById(R.id.text)).setText("Collision");
+        		}
+        		else
+        		{
+            			((TextView)findViewById(R.id.text)).setText("NoCollsion");
+      			}
+		  }
+		
