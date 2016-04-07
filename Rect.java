280bd4fd42fe177724adbf2e0b4a33a4ff47792a
markchr98 <mark4@live.dk>
2016-04-07 13:14:32 +0200

    Rectangle class til alle "figure"

---
+++ Rect.java
@@ -0,0 +1,42 @@
+package com.mark.testcollision;
+
+/**
+ * Created by Mark on 07-04-2016.
+ */
+public class Rect {
+    int width;
+    int height;
+    int x,y;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
